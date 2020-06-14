8b0ff3e096a9381e5d70e633c9e0a7a033063213
Alexander <xanderzinho26@gmail.com>
2020-06-14 01:03:08 -0300

    Fonte arrumada

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -42,7 +42,7 @@ export const SearchHospitalInput = styled.TextInput`
   height: 50px;
   background-color: #fff;
   color: #333;
-  font-family: 'Roboto_400Regular';
+  font-family: ${'Roboto_400Regular'};
   border-radius: 25px;
   padding-left: 20px;
   font-size: 16px;
@@ -77,7 +77,7 @@ export const MarkerContainerImg = styled.Image`
 export const MarkerContainerText = styled.Text`
   flex: 1;
   color: #fff;
-  font-family: 'Roboto_400Regular';
+  font-family: ${'Roboto_400Regular'};
   font-size: 10px;
   line-height: 23px;
 `;
