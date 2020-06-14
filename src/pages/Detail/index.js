3a100ee4c6b588c96e9e07fbd86bf2cfb6b0e220
Alexander <xanderzinho26@gmail.com>
2020-06-14 01:05:27 -0300

    Finalizado parte visual da página

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,25 +1,98 @@
 import React from 'react';
+import { StyleSheet, Image, Linking } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import {
   Wrapper,
-  Container,
-  Header,
-  BalanceContainer,
-  BalanceTitle,
-  Balance,
+  BackHome,
+  HospitalContainer,
+  HospitalTitle,
+  HospitalType,
+  RatingContainer,
+  RatingTitle,
+  RatingType,
+  RatingStar,
+  Footer,
+  Button,
+  ButtonText,
 } from './styles';
 
+import teste from '../../assets/moacyr.jpg';
+
 export default function Detail() {
+  const navigation = useNavigation();
+
+  function handleMaps() {
+    Linking.openURL(`https://www.google.com.br/maps/dir//-22.804,-43.372`);
+  }
+
+  function handlePhone() {
+    Linking.openURL(`tel:+5521979434402`);
+  }
+
   return (
-    <Wrapper>
-      <Container>
-        <Header>
-          <BalanceContainer>
-            <BalanceTitle>Detalhes do Hospital</BalanceTitle>
-            <Balance>Detalhes do hospital</Balance>
-          </BalanceContainer>
-        </Header>
-      </Container>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <BackHome onPress={() => navigation.goBack()}>
+          <MaterialIcons name="arrow-back" size={24} color="#e02041" />
+        </BackHome>
+        <Image style={styles.pointImage} source={teste} />
+        <HospitalContainer>
+          <HospitalTitle>Hospital Municipal Moacyr do Carmo</HospitalTitle>
+          <HospitalType>Hospital geral</HospitalType>
+        </HospitalContainer>
+        <RatingContainer>
+          <RatingTitle>Avaliações</RatingTitle>
+          <RatingType>Limpeza</RatingType>
+          <RatingStar>
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+          </RatingStar>
+
+          <RatingType>Lotação</RatingType>
+          <RatingStar>
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+          </RatingStar>
+
+          <RatingType>Atendimento</RatingType>
+          <RatingStar>
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star" size={24} color="#e02041" />
+            <MaterialIcons name="star-border" size={24} color="#e02041" />
+          </RatingStar>
+        </RatingContainer>
+        <Footer>
+          <Button onPress={handleMaps}>
+            <MaterialIcons name="location-on" size={20} color="#fff" />
+            <ButtonText>Rota</ButtonText>
+          </Button>
+
+          <Button onPress={handlePhone}>
+            <MaterialIcons name="phone" size={20} color="#fff" />
+            <ButtonText>Telefone</ButtonText>
+          </Button>
+        </Footer>
+      </Wrapper>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  pointImage: {
+    width: '100%',
+    height: 170,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
+});
--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -1,36 +1,73 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import { StyleSheet } from 'react-native';
 
 export const Wrapper = styled.SafeAreaView`
   background: #ebebeb;
   flex: 1;
-  padding-top: 30px;
+  padding: 32px;
+  padding-top: ${Constants.statusBarHeight + 30}px;
 `;
 
-export const Container = styled.View`
-  padding-top: 365px;
-  align-items: center;
+export const BackHome = styled.TouchableOpacity``;
+
+export const HospitalContainer = styled.View``;
+
+export const HospitalTitle = styled.Text`
+  font-family: ${'Ubuntu_700Bold'};
+  margin-top: 10px;
+  color: #e02041;
+  font-size: 30px;
 `;
 
-export const Header = styled.View`
-  height: 50px;
-  padding: 0 16px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const BalanceContainer = styled.View``;
-
-export const BalanceTitle = styled.Text`
+export const HospitalType = styled.Text`
+  font-family: ${'Roboto_500Medium'};
+  margin-top: 5px;
   color: #1b1b1b;
-  font-size: 14px;
-  text-align: center;
-  text-transform: uppercase;
+  font-size: 13px;
 `;
-export const Balance = styled.Text`
-  color: #1b1b1b;
-  text-align: center;
+
+export const RatingContainer = styled.View``;
+
+export const RatingTitle = styled.Text`
+  font-family: ${'Ubuntu_700Bold'};
+  margin-top: 40px;
+  color: #e02041;
   font-size: 20px;
-  font-weight: bold;
-  text-transform: uppercase;
+`;
+
+export const RatingType = styled.Text`
+  font-family: ${'Roboto_500Medium'};
+  margin-top: 10px;
+  color: #1b1b1b;
+  font-size: 18px;
+`;
+
+export const RatingStar = styled.View`
+  flex-direction: row;
+`;
+
+export const Footer = styled.View`
+  margin-top: 80px;
+  border-color: #999;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px 32px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 48%;
+  background-color: #e02041;
+  border-radius: 8px;
+  height: 50px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: ${'Roboto_400Regular'};
+  margin-left: 8px;
+  color: #fff;
+  font-size: 16px;
 `;
